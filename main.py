7dde412482c81b6c97ff5759ca56303bf7a21d3b
sara7375-tech <naira7375@gmail.com>
2026-05-19 22:51:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
+class TaskService:
+    def __init__(self, repo: TaskRepository):
+        self.repo = repo
 
-from sklearn import *
-from sklearn.tree import *
-from sklearn.model_selection import *
-from sklearn.metrics import *
+    def create_task(self, task_data: TaskCreate):
+        if task_data.due_date and task_data.due_date < datetime.utcnow():
+            raise InvalidTaskException()
 
-x=[[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
-y=[0,0,1,1,0,1]
-
-a,b,c,d=train_test_split(x,y,test_size=.333333333333333333333333)
-
-t=DecisionTreeClassifier(
-    criterion="gini",
-    splitter="best",
-    max_depth=999999,
-    min_samples_split=2,
-    min_samples_leaf=1,
-    random_state=None
-)
-
-t.fit(a,c)
-
-p=t.predict(b)
-
-print("something:",p)
+        return self.repo.create(task_data)
